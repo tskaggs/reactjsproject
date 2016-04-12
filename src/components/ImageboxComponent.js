22f5aa6cfa29505e22a558a21c5f7c99ca826aa1
Tim Skaggs <me@timskaggs.com>
2016-04-12 13:22:48 -0500

    Image component added

---
+++ src/components/ImageboxComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//Imagebox.sass');
+
+class ImageboxComponent extends React.Component {
+  render() {
+    return (
+      <div className="imagebox-component">
+        Please edit src/components///ImageboxComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+ImageboxComponent.displayName = 'ImageboxComponent';
+
+// Uncomment properties you need
+// ImageboxComponent.propTypes = {};
+// ImageboxComponent.defaultProps = {};
+
+export default ImageboxComponent;
